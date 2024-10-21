019de6eac02cd2728a5911c728c556abc998170d
levan_lazviashvili <l.lazviashvili@mymarket.ge>
2024-10-21 22:58:26 +0400

    queue

--- artisan_queue.sh
+++ artisan_queue.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Define log file path
-LOG_FILE="/home/skiltdbp/api.skillset.ge/cron_log.txt"
+LOG_FILE="/var/wwww/api/cron_log.txt"
 
 # Check if the artisan queue worker is already running
 if ! /usr/bin/pgrep -f "artisan queue:work"; then
     echo "Starting queue worker at $(date)" >> "$LOG_FILE"
-    /usr/local/bin/php /home/skiltdbp/api.skillset.ge/artisan queue:work --timeout=120 --sleep=3 --tries=3 >> "$LOG_FILE" 2>&1
+    /usr/bin/php /var/wwww/api/artisan queue:work --timeout=120 --sleep=3 --tries=3 >> "$LOG_FILE" 2>&1
 fi
